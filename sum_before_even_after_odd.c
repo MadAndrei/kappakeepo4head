905ccde403fe095e345713f58da2cf651c80be4d
holyunchained <42898121+holyunchained@users.noreply.github.com>
2018-10-18 14:43:50 +0300

    Update and rename sumBeforeEvenAfterOdd.c to sum_before_even_after_odd.c

--- sum_before_even_after_odd.c
+++ sum_before_even_after_odd.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "sumBeforeEvenAfterOdd.h"
-#include "indexFirstEven.h"
-#include "indexLastOdd.h"
+#include "sum_before_even_after_odd.h"
+#include "index_first_even.h"
+#include "index_last_odd.h"
 
-int sumBeforeEvenAfterOdd (int *array, int n){
+int sum_before_even_after_odd (int *array, int n){
 	int i;
 	int sumBeforeAfter = 0;
 	for (i=0; i<indexFirstEven(array, n); i++){
