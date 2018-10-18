238091d5154adf8972d792c9902deb62415b2317
holyunchained <42898121+holyunchained@users.noreply.github.com>
2018-10-18 14:18:55 +0300

    Update indexFirstEven.c

--- indexFirstEven.c
+++ indexFirstEven.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "indexFirstEven.h"
-int indexFirstEven(int* array,int n){
+#include "index_first_even.h"
+int index_first_even(int* array,int n){
 	int i=-1;
 	for (i=0; i<n; i++){
 		if (array[i] % 2 == 0)
